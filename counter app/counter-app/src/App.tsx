9b1aa7ae471deb5d955aea7176b7145cc7dc9035
vignes-potharaj <freelancer.nenu@gmail.com>
2025-11-28 01:26:11 +0530

    feat: added 3 buttons 9increase, decrease, reset)

--- counter app/counter-app/src/App.tsx
+++ counter app/counter-app/src/App.tsx
@@ -1,3 +1,28 @@
 import { useState } from 'react'
 import './App.css'
 
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="card">
+        <h1>
+          Count is {count}
+        </h1>
+        <br />
+        <button onClick = {() => setCount((count) => count + 1)}>
+        increase count
+        </button>
+        <button onClick={() => setCount((count) => count - 1)}>
+        decrease count
+        </button>
+        <button onClick={() => setCount((count) => count = 0)}>
+        click To reset
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
